c76ecaa8e5fbf7da684cae768e97e997aef6e74b
Kamil Wyremski <kamil.wyremski@gmail.com>
2020-11-11 11:10:30 -0500

    Pobieranie adresu email z konta Google

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if(!empty($_REQUEST['code'])){
 		$data2 = json_decode(curl_exec($ch), true);
 		if(!empty($data2['email']) and !empty($data2['verified_email'])){
 			$_SESSION['logged_in'] = true;
-			$_SESSION['email'] = $data2['emails'];
+			$_SESSION['email'] = $data2['email'];
 		}
 	}
 }
